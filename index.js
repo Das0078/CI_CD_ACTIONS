c37beb98d01a4c1bf9c8c307c4cd350b3f1dda82
shubhamdas <dasdassd007@gmail.com>
2026-02-04 23:07:00 +0530

    version 1.5 deployed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.json("🚀 Hello from Node.js + Docker 1.4!");
+  res.json("🚀 Hello from Node.js + Docker 1.5!");
 });
 
 app.listen(PORT, () => {
